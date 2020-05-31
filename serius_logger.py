caaf813fa4dbbb8f23efc8433c077a48b214f973
mchiozzi <mchiozzi95@gmail.com>
2020-05-31 01:38:51 -0300

    changed default port to COM1

--- serius_logger.py
+++ serius_logger.py
@@ -6,11 +6,16 @@ from datetime import datetime
 
 file_name = datetime.now().strftime('%Y%m%d%H%M%S') + '.csv'
 
-parser = argparse.ArgumentParser(description='Log Serial Output', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('-p', '--port_device', help='Port where device is connected', default='COM3')
-parser.add_argument('-b', '--baudrate', help='Device baudrate in bps', default=9600, type=int)
-parser.add_argument('-n', '--name', help='Files Output name (default is dynamic)', default=file_name )
-parser.add_argument('-s', '--sep', help='String separator coming from serial', default=';')
+parser = argparse.ArgumentParser(
+    description='Log Serial Output', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('-p', '--port_device',
+                    help='Port where device is connected', default='COM1')
+parser.add_argument('-b', '--baudrate',
+                    help='Device baudrate in bps', default=9600, type=int)
+parser.add_argument(
+    '-n', '--name', help='Files Output name (default is dynamic)', default=file_name)
+parser.add_argument(
+    '-s', '--sep', help='String separator coming from serial', default=';')
 args = parser.parse_args()
 
 port = args.port_device
@@ -21,7 +26,8 @@ sep = args.sep
 serial = Serial(port, baudrate)
 serial.flushInput()
 
-print(f'Starting logger on port {port} with baudrate {baudrate}...Saving on {file_name}!')
+print(
+    f'Starting logger on port {port} with baudrate {baudrate}...Saving on {file_name}!')
 
 while True:
     try:
